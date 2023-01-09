60fb48564f519fa22e30bbcc013f2afdd1b97f0d
Victor Patrick <victorbernardpatrick@gmail.com>
2023-01-09 14:04:39 +0100

    Wrapped React Helmet well for deployment

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
     <HelmetProvider>
+    <BrowserRouter>
     <App />
-    </HelmetProvider>
     </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
